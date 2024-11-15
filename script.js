49df318a44c1d5376dd2ed61e2cc8a081a8b6853
전승민 <seungmin802@gmail.com>
2024-11-15 12:28:08 +0900

    자바스크립트 주석 추가

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//이벤트 기능을 구현할 때 호환성 문제를 해결해 줄 자바스크립 라이브러리 jquer
 jQuery(document).ready(function(){
   $(function() {
     $(".tabmenu>li>a").click(function(){
@@ -7,4 +8,5 @@ jQuery(document).ready(function(){
       return false;
     });
   });
+  //탭 메뉴를 누르면 클래스가 추가되고 기존 클래스 삭제하는 기능
 });
